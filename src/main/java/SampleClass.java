781b20b3f943ea7913baa3de3e8f84c347a68259
Carlos Marti <carlos.marti29@yahoo.com>
2024-12-04 00:25:23 +0000

    Completed Lab

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -63,9 +63,18 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other){
+        if(this.a == other.a && this.b == other.b){
+            return true;
+        }
+
+        return false;
+    }
 
 
     //    implement a custom .toString(){} method here.
-
+    public String toString(){
+        return this.a + " " + this.b;
+    }
     
 }
